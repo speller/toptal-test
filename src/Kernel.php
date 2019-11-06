2dedfefe8a03e17065935792f50e59a990be1d8f
Alex Pravdin <mail@speller.cc>
2019-11-07 07:51:49 +0900

    Backend is almost ready and working with DB.

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,30 +10,45 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
+/**
+ * Symfony application kernel
+ */
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
     const CONFIG_EXTS = '.{php,xml,yaml,yml}';
 
-    public function getCacheDir()
+    /**
+     * @inheritDoc
+     */
+    public function getCacheDir(): string
     {
-        return $this->getProjectDir().'/var/cache/'.$this->environment;
+        return $this->getProjectDir() . '/var/cache/' . $this->environment;
     }
 
-    public function getLogDir()
+    /**
+     * @inheritDoc
+     */
+    public function getLogDir(): string
     {
-        return $this->getProjectDir().'/var/log';
+        return $this->getProjectDir() . '/var/log';
     }
 
-    public function getProjectDir()
+    /**
+     * @inheritDoc
+     */
+    public function getProjectDir(): string
     {
         return __DIR__ . '/../';
     }
 
+    /**
+     * @inheritDoc
+     */
     public function registerBundles()
     {
-        $contents = require $this->getProjectDir().'/config/bundles.php';
+        $contents = require $this->getProjectDir() . '/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if ($envs[$this->environment] ?? $envs['all'] ?? false) {
                 yield new $class();
@@ -41,11 +56,22 @@ class Kernel extends BaseKernel
         }
     }
 
+    /**
+     * @inheritDoc
+     */
+    protected function prepareContainer(ContainerBuilder $container)
+    {
+        $container->registerExtension(new DbExtension());
+        parent::prepareContainer($container);
+    }
+
+    /**
+     * @inheritDoc
+     */
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
     {
-        $confDir = $this->getProjectDir().'/config';
-        $container->registerExtension(new DbExtension());
-        $container->addResource(new FileResource($confDir.'/bundles.php'));
+        $confDir = $this->getProjectDir() . '/config';
+        $container->addResource(new FileResource($confDir . '/bundles.php'));
         $container->setParameter('container.dumper.inline_class_loader', true);
         $revisionFile = $this->getProjectDir() . '/image-tag.txt';
         $container->setParameter(
@@ -55,18 +81,21 @@ class Kernel extends BaseKernel
                 'unknown'
         );
 
-        $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
-        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{packages}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}' . self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir . '/{services}_' . $this->environment . self::CONFIG_EXTS, 'glob');
     }
 
+    /**
+     * @inheritDoc
+     */
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
-        $confDir = $this->getProjectDir().'/config';
+        $confDir = $this->getProjectDir() . '/config';
 
-        $routes->import($confDir.'/{routes}/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, '/', 'glob');
-        $routes->import($confDir.'/{routes}'.self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}/' . $this->environment . '/**/*' . self::CONFIG_EXTS, '/', 'glob');
+        $routes->import($confDir . '/{routes}' . self::CONFIG_EXTS, '/', 'glob');
     }
 }
